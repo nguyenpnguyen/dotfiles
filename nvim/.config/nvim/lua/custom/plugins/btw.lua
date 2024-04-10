60d76d6af2b72f35155d639509875e760778ab71
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-04-10 13:39:06 +0700

    feat(nvim): btw btw

--- nvim/.config/nvim/lua/custom/plugins/btw.lua
+++ nvim/.config/nvim/lua/custom/plugins/btw.lua
@@ -1,17 +1,8 @@
 return {
-  "echasnovski/mini.starter",
-  opts = {
-    items = { { name = "", action = "", section = "" } },
-    header = function()
-      local nonsense = string.format(
-        "%s\n%s\n%s",
-        -- stylua: ignore
-        "I use neovim btw",
-        "and arch as well btw",
-        "with a tiling window manager, btw"
-      )
-      return nonsense
-    end,
-    footer = "btw",
-  },
+  "letieu/btw.nvim",
+  config = function()
+    require('btw').setup({
+      text = "I use Neovim (btw), with Arch (btw),\n and a tiling window manager as well (btw)"
+    })
+  end,
 }
