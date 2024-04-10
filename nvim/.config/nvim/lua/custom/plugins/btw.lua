25811e507a9454e8a7aa7f0d368d96ba882a6929
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-04-10 17:46:58 +0700

    feat(nvim): change btw

--- nvim/.config/nvim/lua/custom/plugins/btw.lua
+++ nvim/.config/nvim/lua/custom/plugins/btw.lua
@@ -2,7 +2,7 @@ return {
   "letieu/btw.nvim",
   config = function()
     require('btw').setup({
-      text = "I use Neovim (btw), with Arch (btw),\n and a tiling window manager as well (btw)"
+      text = "I use Neovim (btw), on Arch (btw),\n with a tiling window manager as well (btw)"
     })
   end,
 }
