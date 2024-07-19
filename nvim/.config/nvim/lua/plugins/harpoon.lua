d7347423b55aafd2c930d3e177963a595150cf4b
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-07-20 02:19:33 +0700

    fix(nvim): Harpoon conflicting keymaps with tmux navigator

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,38 +3,67 @@ return {
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local map = vim.keymap.set
-		local harpoon = require("harpoon")
-		harpoon:setup()
-
-		-- Harpoon keymaps
-		map("n", "<leader>a", function()
-			harpoon:list():add()
-		end, { desc = "Add current file to harpoon" })
-
-		map("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-
-		map("n", "<C-h>", function()
-			harpoon:list():select(1)
-		end)
-		map("n", "<C-j>", function()
-			harpoon:list():select(2)
-		end)
-		map("n", "<C-k>", function()
-			harpoon:list():select(3)
-		end)
-		map("n", "<C-l>", function()
-			harpoon:list():select(4)
-		end)
-
-		-- Toggle previous & next buffers stored within Harpoon list
-		map("n", "<leader>[", function()
-			harpoon:list():prev()
-		end, { desc = "Go to previous buffer in harpoon list" })
-		map("n", "<leader>]", function()
-			harpoon:list():next()
-		end, { desc = "Go to next buffer in harpoon list" })
+		require("harpoon"):setup()
 	end,
+	keys = {
+		{
+			"<leader>a",
+			function()
+				require("harpoon"):list():add()
+			end,
+			desc = "[A]dd current file to harpoon",
+		},
+		{
+			"<C-e>",
+			function()
+				local harpoon = require("harpoon")
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end,
+			desc = "Toggle harpoon quick menu",
+		},
+		{
+			"<leader>h",
+			function()
+				require("harpoon"):list():select(1)
+			end,
+			desc = "Select harpoon list item 1",
+		},
+		{
+			"<leader>j",
+			function()
+				require("harpoon"):list():select(2)
+			end,
+			desc = "Select harpoon list item 2",
+		},
+		{
+			"<leader>k",
+			function()
+				require("harpoon"):list():select(3)
+			end,
+			desc = "Select harpoon list item 3",
+		},
+		{
+			"<leader>l",
+
+			function()
+				require("harpoon"):list():select(4)
+			end,
+			desc = "Select harpoon list item 4",
+		},
+		{
+			"<leader>[",
+			function()
+				require("harpoon"):list():prev()
+			end,
+			desc = "Go to previous buffer in harpoon list",
+		},
+		{
+			"<leader>]",
+
+			function()
+				require("harpoon"):list():prev()
+			end,
+			desc = "Go to next buffer in harpoon list",
+		},
+	},
 }
