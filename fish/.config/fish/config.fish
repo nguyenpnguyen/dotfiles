ae67fc1d704a5711a387f123f14b617729a0fd81
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2026-03-05 15:58:42 +0700

    feat(sway): add slack

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 ~/.local/bin/mise activate fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/nguyen/.local/bin/google-cloud-sdk/path.fish.inc' ]; . '/home/nguyen/.local/bin/google-cloud-sdk/path.fish.inc'; end
